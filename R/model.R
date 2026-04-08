bc20390d0ec6b65078469104e4213617c27cae09
Hiroshi Shimizu <simizu706@gmail.com>
2026-04-08 14:19:12 +0900

    gqをリストで出力を指定できるようにした

--- R/model.R
+++ R/model.R
@@ -145,42 +145,50 @@ model_code <- function(expr, env = parent.frame()) {
 transformed_code <- function(expr, env = parent.frame()) {
   raw_expr <- substitute(expr)
 
-  # 1. コードブロックから「定義されている変数名」を抽出するヘルパー
-  find_assignments <- function(x) {
-    if (is.call(x)) {
-      if (identical(x[[1]], as.name("<-")) || identical(x[[1]], as.name("="))) {
-        # 左辺がシンボル（単一変数）の場合のみ抽出
-        if (is.name(x[[2]])) return(as.character(x[[2]]))
-      }
-      return(unique(unlist(lapply(as.list(x), find_assignments))))
-    }
-    return(NULL)
-  }
-
-  defined_vars <- find_assignments(raw_expr)
-
-  # 2. 中括弧 {} の中身をフラットにする
+  # 1. 中括弧 {} の中身をフラットにする
   if (is.call(raw_expr) && identical(raw_expr[[1]], as.name("{"))) {
     expr_elements <- as.list(raw_expr)[-1]
   } else {
     expr_elements <- list(raw_expr)
   }
 
-  # 3. return(list(var1 = var1, ...)) の形を作成
-  ret_list_args <- lapply(defined_vars, as.name)
-  names(ret_list_args) <- defined_vars
-  ret_call <- as.call(c(list(as.name("list")), ret_list_args))
+  # 2. 最後の要素が list(...) かどうかを判定
+  last_elem <- expr_elements[[length(expr_elements)]]
+  is_explicit_return <- is.call(last_elem) && identical(last_elem[[1]], as.name("list"))
 
-  # 4. 関数ボディの構築
+  if (is_explicit_return) {
+    # 明示的に list(...) が書かれている場合、それを出力として採用
+    ret_call <- last_elem
+    # 重複評価を防ぐため、通常の実行フローから最後の list(...) を削除
+    expr_elements <- expr_elements[-length(expr_elements)]
+
+  } else {
+    # 記述がない場合は従来通り自動抽出する
+    find_assignments <- function(x) {
+      if (is.call(x)) {
+        if (identical(x[[1]], as.name("<-")) || identical(x[[1]], as.name("="))) {
+          if (is.name(x[[2]])) return(as.character(x[[2]]))
+        }
+        return(unique(unlist(lapply(as.list(x), find_assignments))))
+      }
+      return(NULL)
+    }
+    defined_vars <- find_assignments(raw_expr)
+    ret_list_args <- lapply(defined_vars, as.name)
+    names(ret_list_args) <- defined_vars
+    ret_call <- as.call(c(list(as.name("list")), ret_list_args))
+  }
+
+  # 3. 関数ボディの構築
   body_list <- c(
     list(as.name("{")),
     quote(getAll(dat, par)),
-    expr_elements,
+    unname(expr_elements),
     list(as.call(list(as.name("return"), ret_call)))
   )
   body_expr <- as.call(body_list)
 
-  # 5. 関数オブジェクトの生成
+  # 4. 関数オブジェクトの生成
   args <- as.pairlist(alist(dat = , par = ))
   fn_expr <- call("function", args, body_expr)
   fn <- eval(fn_expr, envir = env)
