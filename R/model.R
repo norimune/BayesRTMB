fc9eb3acf3ba165cdd8b99f4283a2a49c3d119e8
norimune <simizu706@norimune.net>
2026-04-08 22:49:47 +0900

    bug fix

--- R/model.R
+++ R/model.R
@@ -815,9 +815,9 @@ rtmb_cor <- function(data, prior = list(lkj_eta = 1.0, mu_sd = 10, sigma_rate = 
   )
 
   params <- list(
-    mu       = Dim(P, names = var_names),
-    sigma    = Dim(P, lower = 0, names = var_names),
-    CF_Omega = Dim(c(P, P), type = "CF_corr", names = var_names)
+    mu       = Dim(P),
+    sigma    = Dim(P, lower = 0),
+    CF_Omega = Dim(c(P, P), type = "CF_corr")
   )
 
   model_expr <- model_code({
