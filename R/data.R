1d6470887f8b04158fd7c1bfcf51441d61be01a5
Hiroshi Shimizu <simizu706@gmail.com>
2026-04-25 10:55:31 +0900

    add data.R

---
+++ R/data.R
@@ -0,0 +1,60 @@
+#' Beverage Preference Data
+#'
+#' @description
+#' A dataset containing preference ratings for various beverages.
+#' This data was used to demonstrate a random effect model in metric
+#' multidimensional unfolding.
+#'
+#' @format A data frame (or matrix) with [INSERT NUMBER OF ROWS] rows and [INSERT NUMBER OF COLUMNS] columns.
+#' \describe{
+#'   \item{Column1}{Description of the first column (e.g., preference score).}
+#'   \item{Column2}{Description of the second column.}
+#'   % Add other columns as necessary
+#' }
+#'
+#' @source
+#' Adachi, K. (2000). A Random Effect Model in Metric Multidimensional Unfolding.
+#' \emph{The Japanese Journal of Behaviormetrics}, 27(1), 12-23.
+#' \url{https://doi.org/10.2333/jbhmk.27.12}
+#'
+#' @keywords datasets
+"beverage"
+
+
+#' Big Five Personality Traits Data
+#'
+#' @description
+#' A dataset containing responses to 20 items measuring the Big Five personality traits.
+#' This dataset was originally collected by the package author.
+#' The items are arranged in a cyclical order representing the five factors:
+#' Extraversion, Neuroticism, Openness, Agreeableness, and Conscientiousness.
+#'
+#' @format A data frame with 10 rows and 20 columns:
+#' \describe{
+#'   \item{BF1}{Extraversion item 1}
+#'   \item{BF2}{Neuroticism item 1}
+#'   \item{BF3}{Openness item 1}
+#'   \item{BF4}{Agreeableness item 1}
+#'   \item{BF5}{Conscientiousness item 1}
+#'   \item{BF6}{Extraversion item 2}
+#'   \item{BF7}{Neuroticism item 2}
+#'   \item{BF8}{Openness item 2}
+#'   \item{BF9}{Agreeableness item 2}
+#'   \item{BF10}{Conscientiousness item 2}
+#'   \item{BF11}{Extraversion item 3}
+#'   \item{BF12}{Neuroticism item 3}
+#'   \item{BF13}{Openness item 3}
+#'   \item{BF14}{Agreeableness item 3}
+#'   \item{BF15}{Conscientiousness item 3}
+#'   \item{BF16}{Extraversion item 4}
+#'   \item{BF17}{Neuroticism item 4}
+#'   \item{BF18}{Openness item 4}
+#'   \item{BF19}{Agreeableness item 4}
+#'   \item{BF20}{Conscientiousness item 4}
+#' }
+#'
+#' @source
+#' Originally collected by the package author.
+#'
+#' @keywords datasets
+"BigFive"
