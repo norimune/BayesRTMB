1befb5074efd7f20f7ebf9f8be35ae71bc56edc6
Hiroshi Shimizu <simizu706@gmail.com>
2026-05-04 10:55:24 +0900

    feat: add comprehensive vignettes, wrapper functions, and example data to BayesRTMB

--- R/data.R
+++ R/data.R
@@ -63,26 +63,26 @@
 #' @keywords datasets
 "BigFive"
 
-#' Group Discussion Simulation Data
+#' Debate Simulation Data
 #'
 #' @description
 #' A simulated dataset containing individual satisfaction scores and related
-#' variables from a 3-person group discussion experiment. This dummy data was
+#' variables from a 3-person group debate experiment. This dummy data was
 #' created by the package author to demonstrate hierarchical (multilevel) modeling.
 #'
 #' @format A data frame with 300 rows and 6 columns:
 #' \describe{
 #'   \item{group}{Group identifier. Each group consists of 3 members.}
-#'   \item{satisfaction}{Self-reported satisfaction level of the individual after the discussion.}
-#'   \item{talk}{Amount of talk or utterances by the individual during the discussion.}
-#'   \item{performance}{Evaluated performance of the group discussion (group-level variable).}
+#'   \item{sat}{Self-reported satisfaction level of the individual after the debate.}
+#'   \item{talk}{Amount of talk or utterances by the individual during the debate.}
+#'   \item{perf}{Evaluated performance of the group debate (group-level variable).}
 #'   \item{skill}{Evaluated conversation skill of the individual.}
-#'   \item{condition}{Experimental condition indicator (e.g., 0 = control, 1 = treatment).}
+#'   \item{cond}{Experimental condition indicator (e.g., 0 = control, 1 = treatment).}
 #' }
 #'
 #' @source
 #' Simulated dummy data created by the package author.
 #'
 #' @keywords datasets
-"discussion"
+"debate"
 
